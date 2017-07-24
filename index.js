d14550264f68d9a55095fe42e0886a5ee1df14a7
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-24 18:49:02 -0400

    Added middleware, and wrote code to start the server

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+
+//setup express applicaiton
+var app = express();
+//add imported middleware stuff
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+    res.end(req.body);
+});
+
+
+app.listen(8080, function() {
+    console.log("The Server is Currently Running.");
+});
