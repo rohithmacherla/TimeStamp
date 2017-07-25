6a333c2e1b5097b2a4acc150e69a579786cfe0ca
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-24 21:24:49 -0400

    The server corrects outputs unix and natural language time when input is in natural language form

--- index.js
+++ index.js
@@ -6,13 +6,37 @@ var cors = require('cors');
 //setup express applicaiton
 var app = express();
 //add imported middleware stuff
-app.use(bodyParser.json());
-app.use(cors());
+//app.use(bodyParser.json());
+//app.use(cors());
 
-app.get('/', function(req, res) {
-    res.end(req.body);
+app.get('/',function(req, res) {
+    res.end("Hello World");
 });
 
+
+app.get('/:id', function(req, res) {
+    var id = req.params.id;
+    var obj = {};
+    if(isNaN(id)) {
+        //check if valid natural language date
+        //if not valid, return null.
+        //if valid then output unix and natrual language time
+        var array_values = id.split(' ');
+        var month = array_values[0];
+        var day = array_values[1];
+        var year = array_values[2];
+        obj.natural = month + " " + day +", " + year;
+        obj.unix = new Date(obj.natural).getTime();
+    } else {
+        //check if valid unix time stamp
+        //if not valid return null
+        //if valid, then output unix and natural language time 
+
+    }
+
+
+    res.json(obj);
+});
 
 app.listen(8080, function() {
     console.log("The Server is Currently Running.");
